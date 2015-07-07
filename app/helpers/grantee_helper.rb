235b040582d1e01bc1f45929a64627a8c1162ee1
Stephen Corwin <corwinstephen@gmail.com>
2015-07-07 13:48:18 -0700

    Grantees page

--- app/helpers/grantee_helper.rb
+++
@@ -1,5 +0,0 @@
-module GranteeHelper
-  def grantee_project_name
-    @grantee.project_name
-  end
-end
--- app/models/grantee.rb
+++ app/models/grantee.rb
@@ -1,5 +1,5 @@
 class Grantee < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "600x600>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates_presence_of :project_name, :year
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
