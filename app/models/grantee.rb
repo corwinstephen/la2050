54ff5521083e1ad77f8192f36deed6302ed2ad81
Stephen Corwin <corwinstephen@gmail.com>
2015-07-17 12:12:30 -0700

    Can't remember what these changes are. Oh well.

--- app/models/grantee.rb
+++ app/models/grantee.rb
@@ -1,6 +1,5 @@
 class Grantee < ActiveRecord::Base
   validates_presence_of :project_name, :year
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   has_many  :goal_taggings,
             as: :taggable
@@ -9,4 +8,5 @@ class Grantee < ActiveRecord::Base
             through: :goal_taggings
 
   has_attached_file :photo, :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
