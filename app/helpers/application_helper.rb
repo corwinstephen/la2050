f1d31854796a0fbfdf847d92ef4e305f05c6b3d6
Stephen Corwin <corwinstephen@gmail.com>
2015-07-20 13:17:22 -0700

    Metrics nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
   end
 
   def display_dashboard_nav?
-    @cms_page.present? && ['goals', 'live', 'play', 'learn', 'connect', 'create', 'metrics', 'datasets', 'reports', 'dashboard'].include?(@cms_page.slug)
+    (@cms_page.present? && ['goals', 'live', 'play', 'learn', 'connect', 'create', 'metrics', 'datasets', 'reports', 'dashboard'].include?(@cms_page.slug)) ||
+      controller.controller_name == 'metrics'
   end
 
   def display_subnav?
