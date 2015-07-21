9613c68510b777298ea9e7864df1234f9a64ac04
Vyki <vyki@nationbuilder.com>
2015-07-21 14:59:56 -0700

    update grantees table, add organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720231601) do
+ActiveRecord::Schema.define(version: 20150721212133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,12 @@ ActiveRecord::Schema.define(version: 20150720231601) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.integer  "organization_id"
+    t.string   "website_url"
+    t.string   "submission_url"
+    t.string   "instagram"
+    t.string   "twitter"
+    t.string   "facebook"
   end
 
   create_table "jobs", force: :cascade do |t|
