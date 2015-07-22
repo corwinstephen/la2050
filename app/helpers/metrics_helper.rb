a309cd4c7e670e12ac3ddd98009819d9e3cac778
Stephen Corwin <corwinstephen@gmail.com>
2015-07-22 12:21:11 -0700

    Goal filtes

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -1,2 +1,9 @@
 module MetricsHelper
+  def metrics_from_params
+    if params[:goal].present?
+      Metric.includes(:goals).joins(:goals).where("goals.name = ?", params[:goal])
+    else
+      Metric.all
+    end
+  end
 end
