f51702bef9d3abc86135b1e14318cec4750cb898
Stephen Corwin <corwinstephen@gmail.com>
2015-07-23 15:16:14 -0700

    get action items to use s3

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -6,7 +6,15 @@ class ActionItem < ActiveRecord::Base
             through: :goal_taggings
 
 
-  has_attached_file :photo, :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo,
+                    :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :path => '/action_items/:id/:style/:filename',
+                    :default_url => "/images/:style/missing.png",
+                    :url => ":s3_domain_url",
+                    :s3_host_name => "s3-us-west-1.amazonaws.com"
+
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   validates_presence_of :title,
