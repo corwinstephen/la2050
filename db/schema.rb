fad6d31abd0c2ef89c1efd2dddc36097f3cccac2
Vyki <vyki@nationbuilder.com>
2015-08-31 23:32:27 -0700

    display reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723194915) do
+ActiveRecord::Schema.define(version: 20150901060450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -273,6 +273,14 @@ ActiveRecord::Schema.define(version: 20150723194915) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "report_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
