d517f9084af0bb14d66a05840ceccfa13c04ef8d
Vyki <vyki@nationbuilder.com>
2015-09-03 16:05:20 -0700

    add helpers for voting and winner announce dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,29 @@ module ApplicationHelper
   def challenge_open?
     # Challenge Opens September 8th 2015 12noon PST
     open_at = DateTime.new(2015,9,8,12,00,00,"-08:00")
-    # Challenge Opens October 6th 2015 12noon PST
+    # Challenge Closes October 6th 2015 12noon PST
     close_at = DateTime.new(2015,10,6,12,00,00,"-08:00")
     today = DateTime.now
     today.between? close_at, open_at
   end
-  
+
+  def voting_open?
+    # Voting Opens October 20th 2015 12noon PST
+    open_at = DateTime.new(2015,10,20,12,00,00,"-08:00")
+    # Voting Closes November 3rd 2015 12noon PST
+    close_at = DateTime.new(2015,11,3,12,00,00,"-08:00")
+    today = DateTime.now
+    today.between? close_at, open_at
+  end
+
+  # if today is after vote date show winners
+  def winners_next?
+    # Winners December 8th 2015 12noon PST
+    vote_close_at = DateTime.new(2015,11,3,12,00,00,"-08:00")
+    today = DateTime.now
+    today > vote_close_at
+  end
+
   def display_about_nav?
     request.original_fullpath == '/blog' ||
       @cms_page.present? && ['about', 'press', 'instagram', 'community'].include?(@cms_page.slug)
