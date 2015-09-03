7a3040f8948b67d24e3688fc4448e23c54d840c1
Vyki <vyki@nationbuilder.com>
2015-09-03 14:41:15 -0700

    use helper to display hero text only if challenges open

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
   include AutoHtml
-  
-  def current_date
-    DateTime.now
-  end
 
-  def challenge_open_date
+  def challenge_open?
     # Challenge Opens September 8th 2015 12noon PST
-    DateTime.new(2015,9,8,12,Rational(-8,24))
+    open_at = DateTime.new(2015,9,8,12,00,00,"-08:00")
+    # Challenge Opens October 6th 2015 12noon PST
+    close_at = DateTime.new(2015,10,6,12,00,00,"-08:00")
+    today = DateTime.now
+    today.between? close_at, open_at
   end
-
+  
   def display_about_nav?
     request.original_fullpath == '/blog' ||
       @cms_page.present? && ['about', 'press', 'instagram', 'community'].include?(@cms_page.slug)
