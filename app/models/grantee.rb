fb4035ac31bd7fc4f2fdc69b8efd127d3b65497b
Stephen Corwin <corwinstephen@gmail.com>
2015-09-04 12:19:20 -0400

    Make grantee photos use aws

--- app/models/grantee.rb
+++ app/models/grantee.rb
@@ -9,7 +9,15 @@ class Grantee < ActiveRecord::Base
 
   has_and_belongs_to_many :organizations
 
-  has_attached_file :photo, :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" }, :default_url => "grantees/missing.svg"
+  has_attached_file :photo, 
+                    :styles => { :medium => "600x600>", :small => "360x215#", :thumb => "100x100>" },
+                    :path => '/grantees/:id/photos/:style/:filename',
+                    :default_url => "grantees/missing.svg",
+                    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :url => ":s3_domain_url",
+                    :s3_host_name => "s3-us-west-1.amazonaws.com"
+
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   attr_accessor :delete_photo
